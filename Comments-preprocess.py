0a37017c0367562a5051ab9defa80682c50ec8ef
akshat35 <67780467+akshat35@users.noreply.github.com>
2020-07-15 14:27:24 +0530

    Add files via upload

---
+++ Comments-preprocess.py
@@ -0,0 +1,42 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import pandas as pd
+import re
+
+Comments = pd.read_csv("dataset.csv")
+Rev_comm=Comments["text"]
+
+def preprocess(text):
+    clean_data = []
+    for x in (text[:]): 
+        new_text = re.sub('<.*?>', '', x)   # remove HTML tags
+        new_text = re.sub(r'[^\w\s]', '', new_text) # remove punc.
+        new_text = re.sub(r'\d+','',new_text)# remove numbers
+        new_text = new_text.lower() # lower case, .upper() for upper          
+        if new_text != '':
+            clean_data.append(new_text)
+    return clean_data
+
+def tokenization_w(words):
+    w_new = []
+    for w in (words[:]):
+        w_token = word_tokenize(w)
+        if w_token != '':
+            w_new.append(w_token)
+    return w_new
+
+lemmat = WordNetLemmatizer()
+def lemmatization(words):
+    new = []
+    for i in range(len(Rev_comm)):
+        lem_words = [lemmat.lemmatize(x) for x in (words[:][i])]
+        new.append(lem_words)
+    return new
+
+clean_comments=preprocess(Comments)
+comm_words=tokenization_w(clean_comments)
+lem=lemmatization(comm_words)
+print(lem)
